82998d09f2e1f6f069692f9190ad59b040b16193
hakimhassani97 <hakimhassani97@gmail.com>
2020-03-18 16:51:44 +0100

    added map class

---
+++ src/Data/MapAPI.js
@@ -0,0 +1,32 @@
+export default class MapAPI{
+    // var map = L.map('map').setView([36.7525, 3.042], 11.5);
+    // $(document).ready(()=>{
+    //     initMap(36.7525, 3.042)
+    // })
+    // const initMap=(lat, lon)=>{
+    //     map.setView([lat, lon]);
+    //     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    //     }).addTo(map);
+    //     drawDataOnMap(lat, lon)
+    //     map.on('click', onMapClick);
+    // }
+    // const drawDataOnMap=(lat, lon)=>{
+    //     var greenIcon = L.icon({
+    //         iconUrl: 'gps.png',
+    //         // shadowUrl: 'leaf-shadow.png',
+    //         iconSize: [38, 95],
+    //         shadowSize: [50, 64],
+    //         iconAnchor: [22, 94],
+    //         shadowAnchor: [4, 62],
+    //         popupAnchor: [-3, -76]
+    //     });
+    //     $('.leaflet-marker-pane').html('')
+    //     var c=L.marker([lat, lon], {icon: greenIcon})
+    //     c.addTo(map);
+    //     c.bindPopup(currentInfo);
+    // }
+    // const onMapClick=(e)=>{
+    //     getWeatherForCity('&lat='+e.latlng.lat+'&lon='+e.latlng.lng)
+    // }
+}
